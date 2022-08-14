07a4c6e2278b84b9ea337c0a19c88a3526633a4f
John Malapit <johncmalapit@gmail.com>
2022-08-14 17:02:43 -0400

    add react-router-dom pacage, routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+// import LandingPage from "./components/LandingPage";
 
 import MapComponent from "./components/MapComponent";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Commuter Pal</h1>
-      <MapComponent />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MapComponent />} />
+        <Route path="/login" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
