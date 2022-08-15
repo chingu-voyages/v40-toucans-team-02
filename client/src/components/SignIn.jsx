6a8c0a1725a5e59a777116d7e8bfeaa37d747693
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-08-15 17:15:02 -0400

    Update SignIn.jsx
    updated database to have empty strings

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -14,12 +14,12 @@ function SignIn () {
   // User Login info
   const database = [
     {
-      username: "user1",
-      password: "pass1"
+      username: " ",
+      password: " "
     },
     {
-      username: "user2",
-      password: "pass2"
+      username: " ",
+      password: " "
     }
   ];
 
