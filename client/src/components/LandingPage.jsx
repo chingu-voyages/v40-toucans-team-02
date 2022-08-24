8278d14d3e5ec4f7da258e16565865ae48bc1e0d
John Malapit <johncmalapit@gmail.com>
2022-08-23 20:53:55 -0400

    add welcome message, change logout link to logout button

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import MapComponent from "./MapComponent";
+import jwt_decode from "jwt-decode";
 
 const LandingPage = ({ setIsLoggedIn }) => {
+  const authToken = localStorage.getItem("CommuterPalAuthToken");
+  const { username } = jwt_decode(authToken);
+
   const handleLogout = () => {
     localStorage.removeItem("CommuterPalAuthToken");
     setIsLoggedIn(false);
   };
   return (
     <div>
-      <h1>Commuter Pal</h1>
-      <nav>
-        <a href="#" onClick={handleLogout}>
-          logout
-        </a>
-      </nav>
+      <h1>{`Welcome ${username}`}</h1>
+      <button onClick={handleLogout}>logout</button>
       <MapComponent />
     </div>
   );
